cdf0b7d68a1fabf2d7342467507e7458087baabe
Airen <nyzrus@gmail.com>
2021-01-20 11:24:48 -0800

    Depth 1 crawl not working for certain relative path

--- cli.js
+++ cli.js
@@ -1,9 +1,8 @@
 const stdin = process.openStdin();
-const fs = require('fs');
 const axios = require('axios');
 require('dotenv').config();
-const Path = require('path');
 const cheerio = require('cheerio');
+const URL = require('url-parse');
 
 console.log('Ready to crawl....');
 
@@ -11,21 +10,48 @@ stdin.addListener('data', async function (url) {
 	crawl(url);
 });
 
-const visitedLinks = [];
+const pagesToVisit = [];
+const visitedPages = {};
+var urlObject = null;
+var baseUrl = '';
+const workingDirectory = '';
+
+function getUrlDetails(stringifiedUrl) {
+	urlObject = new URL(stringifiedUrl);
+	baseUrl = urlObject.protocol + '//' + urlObject.hostname;
+	//baseUrl = stringifiedUrl.substring(0, stringifiedUrl.lastIndexOf('/'));
+}
+
+function createUrlFromRelativePath() {}
 
 async function crawl(url) {
 	let stringifiedUrl = url + '';
-	return await axios
-		.get(stringifiedUrl)
-		.then((response) => {
-			let $ = cheerio.load(response.data);
-			getRelativeLinksFromPage($);
-			searchForPhoneNumbers($);
-		})
-		.catch((error) => {
-			error.status = (error.response && error.response.status) || 500;
-			throw error;
-		});
+	getUrlDetails(stringifiedUrl);
+	pagesToVisit.push(stringifiedUrl);
+
+	while (pagesToVisit.length > 0) {
+		let currentUrl = pagesToVisit.pop();
+		if (currentUrl in visitedPages) {
+			console.log('Skipping ' + currentUrl);
+			continue;
+		} else {
+			visitedPages[currentUrl] = true;
+			console.log('Visiting ' + currentUrl);
+			await axios
+				.get(currentUrl)
+				.then((response) => {
+					let $ = cheerio.load(response.data);
+					getRelativeLinksFromPage($);
+					searchForPhoneNumbers($);
+				})
+				.catch((error) => {
+					error.status = (error.response && error.response.status) || 500;
+					throw error;
+				});
+			visitedPages[currentUrl] = currentUrl;
+		}
+	}
+	console.log('Complete!');
 }
 
 async function searchForPhoneNumbers($) {
@@ -34,12 +60,25 @@ async function searchForPhoneNumbers($) {
 	let regex = /(?:[-+() ]*\d){10,13}/g;
 
 	const array = [...bodyText.matchAll(regex)];
-	//console.log(array);
+	console.log(array);
 }
 
 function getRelativeLinksFromPage($) {
 	let relativeLinks = $('a[href]');
+	const list = [];
 	relativeLinks.each(function () {
-		console.log($(this).attr('href'));
+		let token = $(this).attr('href');
+		if (token[0] !== '/') {
+			token = '/' + token;
+		}
+		pagesToVisit.push(baseUrl + token);
 	});
 }
+
+/**
+ * https://therecount.github.io/interview-materials/project-a/1.html
+ * https://therecount.github.io/interview-materials/project-a/2.html
+ *  - 2.html
+ * https://therecount.github.io/interview-materials/project-a/3.html
+ *  - /interview-materials/project-a/3.html
+ */
