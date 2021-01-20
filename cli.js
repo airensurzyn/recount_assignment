8e0ab59f25b890faec5855173b1dbb9de6d27749
Airen <nyzrus@gmail.com>
2021-01-20 10:15:02 -0800

    Working regex parse of single web page

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const Path = require('path');
 const cheerio = require('cheerio');
 const { default: Axios } = require('axios');
+const extractor = require('phone-number-extractor');
 
 const exec = require('child_process').exec;
 console.log('Ready to crawl....');
@@ -18,9 +19,7 @@ async function crawl(url) {
 	return await axios
 		.get(stringifiedUrl)
 		.then((response) => {
-			let cheerio_data = cheerio.load(response.data);
-			searchForPhoneNumbers(cheerio_data);
-			//console.log(response.data);
+			searchForPhoneNumbers(cheerio.load(response.data));
 		})
 		.catch((error) => {
 			error.status = (error.response && error.response.status) || 500;
@@ -28,20 +27,10 @@ async function crawl(url) {
 		});
 }
 
-async function searchForPhoneNumbers(data) {
-	var bodyText = data('html > body').text().toLowerCase();
-	console.log(data.html());
-	/*console.log(bodyText);
-	var phoneNumberRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-	for (let i = 0; i < bodyText.length; i++) {
-		if (parseInt(bodyText[i]) >= '0' || parseInt(bodyText[i]) <= '9') {
-			console.log(bodyText[i]);
-			console.log(bodyText.substring(i, i + 11).match(phoneNumberRegex));
-		}
-	}
-	console.log(bodyText.match(phoneNumberRegex));*/
-}
+async function searchForPhoneNumbers($) {
+	var bodyText = $('html > body').text();
+	let regex = /(?:[-+() ]*\d){10,13}/g;
 
-/**
- * ^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
- */
+	const array = [...bodyText.matchAll(regex)];
+	console.log(array);
+}
